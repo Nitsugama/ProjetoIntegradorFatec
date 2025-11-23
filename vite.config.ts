a70c53e5fc8211f97f1a58c6e7e5999903814b77
Nicolas <pinhatapinha@gmail.com>
2025-11-23 16:59:33 -0300

    Configuracoes de deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
+    base: "/projetointegradorfatec",
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
